78ad6e2284e7f425e44c01cf1d511dc43107206b
Farid Amrahov <91734370+faridamrah@users.noreply.github.com>
2023-06-13 13:51:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Docker!');
+  res.send('Hello, World!');
 });
 
 app.listen(3000, () => {
